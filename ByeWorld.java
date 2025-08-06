cf623788731400ed642950afbd5ba252f5d305d2
Aidanjosiah02 <aidanjosiah02@gmail.com>
2025-08-05 17:35:18 -0700

    Added a random number generator to ByeWorld.java

--- ByeWorld.java
+++ ByeWorld.java
@@ -1,5 +1,10 @@
+import java.util.Random;
+
 public class ByeWorld {
     public static void main(String[] args) {
         System.out.println("Bye, World!");
+
+        Random generator = new Random();
+        System.out.println(generator.nextInt(0,256));
     }
 }
