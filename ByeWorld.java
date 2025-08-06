c5da94eae12619558fd95d0430d4b1a83621eae4
Katominh <jasonandkhangminh@gmail.com>
2025-08-05 17:36:31 -0700

    Edited java file.

--- ByeWorld.java
+++ ByeWorld.java
@@ -1,5 +1,14 @@
+static final int MAX = 5;
+
 public class ByeWorld {
     public static void main(String[] args) {
-        System.out.println("Bye, World!");
+        System.out.println("Byeeeeeeeee World!");
+        bye();
+    }
+
+    private static void bye() {
+        for (int i = 0; i < MAX; i++) {
+            System.out.println("Goodbye World!");
+        }
     }
 }
